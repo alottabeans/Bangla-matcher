73e205cb7142dab4e6afd33c499f135dcaa07cca
alottabeans <50392507+alottabeans@users.noreply.github.com>
2020-01-07 12:42:14 -0500

    Add files via upload

--- banglogic.py
+++ banglogic.py
@@ -5,25 +5,28 @@ with open('bangladata.json') as f:
     data = json.load(f)
 
 bw = data['bangla_words'] #bangla words
-
 tw = data['translations'] #translated words
 
+#convert lists to dict
 banglalist = dict(zip(bw, tw))
 
 gen_question = random.choice(list(banglalist.keys()))
 
-#random buttons
-gen_btn1 = random.choice(list(banglalist.values()))
-gen_btn2 = random.choice(list(banglalist.values()))
-gen_btn3 = random.choice(list(banglalist.values()))
-gen_btn4 = banglalist[gen_question]
+#random answers
+wrong_btn1 = random.choice(list(banglalist.values()))
+wrong_btn2 = random.choice(list(banglalist.values()))
+wrong_btn3 = random.choice(list(banglalist.values()))
+correct_btn = banglalist[gen_question]
 
-mult_choice_btn = [gen_btn1, gen_btn2, gen_btn3, gen_btn4]
+mult_choice = [wrong_btn1, wrong_btn2, wrong_btn3, correct_btn]
 
-shuffle_btn = random.shuffle(mult_choice_btn)
+#shuffle buttons 
+shuffle_choices = random.shuffle(mult_choice)
 
-def check_if_correct(ins):
-    if ins.text == gen_btn4:
-        print("correct!")
-    else:
-        print("wrong") 
+def check_if_correct(ins, ques): 
+     
+    if ins.text == correct_btn:
+        ques.color=[0, 0.99, 0, 1]
+
+    elif ins.text != correct_btn:
+        ques.color=[0.99, 0, 0, 1]
--- bangui.py
+++ bangui.py
@@ -9,51 +9,56 @@ from kivy.uix.stacklayout import StackLayout
 from kivy.properties import NumericProperty
 from kivy.config import Config
 
-#disable window resizing
 Config.set('graphics', 'resizable', False)
 
 from banglogic import *
 
+#TODO: add score label
 
-def bangla_app():      
-    root = StackLayout(orientation='lr-bt')
+class BanglaGame:
+
+    def bangla_game(self):      
+        root = StackLayout(orientation='lr-bt')
+        
+        #generated question    
+        self.question = Label(text=gen_question, 
+                font_name='fonts/Hind_Siliguri/HindSiliguri-Medium.ttf', font_size='50sp') 
+
+        btn1 = Button(text=mult_choice[0], size_hint=(1, .1)) 
+        btn1.bind(on_press=self.callback)
+
+        btn2 = Button(text=mult_choice[1], size_hint=(1, .1)) 
+        btn2.bind(on_press=self.callback)
+
+        btn3 = Button(text=mult_choice[2], size_hint=(1, .1))
+        btn3.bind(on_press=self.callback)
+
+        btn4 = Button(text=mult_choice[3], size_hint=(1, .1))  
+        btn4.bind(on_press=self.callback)
+        
+        #add buttons 
+        root.add_widget(btn1)
+        root.add_widget(btn2) 
+        root.add_widget(btn3)
+        root.add_widget(btn4)
+        
+        # add question label
+        root.add_widget(self.question) 
+
+        return root
     
-    question = Label(text=gen_question, 
-            font_name='fonts/Hind_Siliguri/HindSiliguri-Medium.ttf', font_size='50sp') 
-    
-    btn1 = Button(text=mult_choice_btn[0], size_hint=(1, .1)) 
-    btn1.bind(on_press=callback)
-
-    btn2 = Button(text=mult_choice_btn[1], size_hint=(1, .1)) 
-    btn2.bind(on_press=callback)
-
-    btn3 = Button(text=mult_choice_btn[2], size_hint=(1, .1))
-    btn3.bind(on_press=callback)
-
-    btn4 = Button(text=mult_choice_btn[3], size_hint=(1, .1))  
-    btn4.bind(on_press=callback)
-     
-    #add buttons 
-    root.add_widget(btn1)
-    root.add_widget(btn2) 
-    root.add_widget(btn3)
-    root.add_widget(btn4)
-    
-    # add question label
-    root.add_widget(question) 
-
-    return root
+ 
+    def callback(self, instance):
+         check_if_correct(instance, self.question) 
 
 
 class Banglapp(App):
-
+    
     def build(self):
-        return bangla_app()
-
-
-def callback(instance):
-    check_if_correct(instance) 
+        bg = BanglaGame() 
+        return bg.bangla_game()
 
 
 if __name__ == '__main__':
     Banglapp().run()
+
