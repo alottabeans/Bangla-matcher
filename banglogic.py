75f25fd0c99baaa3e5257c599e4b58f26531b6cb
alottabeans <50392507+alottabeans@users.noreply.github.com>
2020-02-02 13:49:36 -0500

    Add files via upload

--- banglogic.py
+++ banglogic.py
@@ -14,17 +14,16 @@ class BangLogic:
     
     def __init__(self):
         self.gen_question = random.choice(list(bangladict.keys()))
-
+         
         self._btn1 = random.choice(list(bangladict.values())) 
         self._btn2 = random.choice(list(bangladict.values())) 
         self._btn3 = random.choice(list(bangladict.values())) 
-        self._btn4 = random.choice(list(bangladict.values()))
-
         self.correct_btn = bangladict[self.gen_question]
+
         self.mult_choice = [self._btn1, self._btn2, self._btn3, self.correct_btn] 
 
     
-    def check_if_correct(self, _instance, _lbl, _btn_list):     
+    def check_if_correct(self, _instance, _lbl, _btn_list):      
         if _instance.text == self.correct_btn:
             _lbl.color = [0, 0.99, 0, 1]                    
             self.update_buttons(_btn_list)
@@ -33,14 +32,16 @@ class BangLogic:
         elif _instance.text != self.correct_btn:
             _lbl.color = [0.99, 0, 0, 1] 
             self.update_buttons(_btn_list)
+            self.update_label(_lbl)
+            print(self.correct_btn, self.gen_question)
 
 
     def update_label(self, gui_lbl):
-        self.gen_question = random.choice(list(bangladict.keys()))
+        self.gui_lbl = gui_lbl 
         self.gui_lbl.text = self.gen_question
 
-
     def update_buttons(self, _gui_btns): 
+        self._gui_btns = _gui_btns 
         for btn in _gui_btns[:-1]:
-            btn.text = random.choice(list(bangladict.values()))   
-        _gui_btns[3] = self.correct_btn 
+            btn = random.choice(list(bangladict.values()))
+        shuffle_btns = random.shuffle(self._gui_btns) 
--- bangui.py
+++ bangui.py
@@ -2,51 +2,59 @@ import kivy
 
 kivy.require('1.11.1')
 
+from kivy.config import Config
+
+Config.set('graphics', 'resizable', False)
+Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
+
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.stacklayout import StackLayout
-from kivy.properties import NumericProperty
 from kivy.config import Config
 
 Config.set('graphics', 'resizable', False)
 
-from banglogic import *
+from banglogic import BangLogic
+
 
 
 class BanglaGame:
-
-    def bangla_game(self):
+    
+    bl = BangLogic() 
+    
+    def bangla_game(self):     
+        
         root = StackLayout(orientation='lr-bt')
         
-        self.question = Label(text=gen_question, 
+        self.ques_lbl = Label(text=self.bl.gen_question, 
                 font_name='fonts/Hind_Siliguri/HindSiliguri-Medium.ttf', font_size='50sp') 
-
-        self.btn1 = Button(text=mult_choice[0], size_hint=(1, .1)) 
+        
+        self.btn1 = Button(text=self.bl.mult_choice[0], size_hint=(1, .1)) 
         self.btn1.bind(on_press=self.callback)
 
-        self.btn2 = Button(text=mult_choice[1], size_hint=(1, .1)) 
+        self.btn2 = Button(text=self.bl.mult_choice[1], size_hint=(1, .1)) 
         self.btn2.bind(on_press=self.callback)
 
-        self.btn3 = Button(text=mult_choice[2], size_hint=(1, .1))
+        self.btn3 = Button(text=self.bl.mult_choice[2], size_hint=(1, .1))
         self.btn3.bind(on_press=self.callback)
 
-        self.btn4 = Button(text=mult_choice[3], size_hint=(1, .1))  
+        self.btn4 = Button(text=self.bl.mult_choice[3], size_hint=(1, .1))  
         self.btn4.bind(on_press=self.callback)
+        
+        self.btn_list = [self.btn1, self.btn2, self.btn3, self.bl.correct_btn]
         
         root.add_widget(self.btn1)
         root.add_widget(self.btn2) 
         root.add_widget(self.btn3)
         root.add_widget(self.btn4)
         
-        self.btn_list = [self.btn1, self.btn2, self.btn3, self.btn4]
-
-        root.add_widget(self.question) 
+        root.add_widget(self.ques_lbl) 
         
         return root 
  
     def callback(self, instance):
-        check_if_correct(instance, self.question, self.btn_list) 
+        self.bl.check_if_correct(instance, self.ques_lbl, self.btn_list) 
 
 class Banglapp(App):
     
