c5966dc11cc2e425c15e83e09e3a345382dfeedf
alottabeans <50392507+alottabeans@users.noreply.github.com>
2020-02-03 09:43:08 -0500

    Add files via upload

--- bangui.py
+++ bangui.py
@@ -11,12 +11,9 @@ from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.stacklayout import StackLayout
-from kivy.config import Config
-
-Config.set('graphics', 'resizable', False)
+from kivy.properties import StringProperty
 
 from banglogic import BangLogic
-
 
 
 class BanglaGame:
@@ -42,7 +39,7 @@ class BanglaGame:
         self.btn4 = Button(text=self.bl.mult_choice[3], size_hint=(1, .1))  
         self.btn4.bind(on_press=self.callback)
         
-        self.btn_list = [self.btn1, self.btn2, self.btn3, self.bl.correct_btn]
+        self.btn_list = [self.btn1, self.btn2, self.btn3, self.btn4]
         
         root.add_widget(self.btn1)
         root.add_widget(self.btn2) 
@@ -51,10 +48,14 @@ class BanglaGame:
         
         root.add_widget(self.ques_lbl) 
         
+
+        self.btn_list = [self.btn1, self.btn2, self.btn3, self.btn4]
+        
         return root 
  
     def callback(self, instance):
         self.bl.check_if_correct(instance, self.ques_lbl, self.btn_list) 
+
 
 class Banglapp(App):
     
@@ -65,3 +66,4 @@ class Banglapp(App):
 
 if __name__ == '__main__':
     Banglapp().run()
+
