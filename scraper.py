fda85ff651aaa9ea25f78e79370b589e4d5a6857
alottabeans <50392507+alottabeans@users.noreply.github.com>
2020-06-01 06:57:38 -0400

    Add files via upload

---
+++ scraper.py
@@ -0,0 +1,28 @@
+from lxml import etree, html
+import requests
+import json
+
+#TODO: get scraper to work
+class Scrape:  
+  def __init__(self, URL):
+    self.URL = URL
+    self.URL = input('Enter URL: ')
+
+    r = requests.get(URL)
+
+    self.tree = html.fromstring(r.text)
+    self.tree = etree.ElementTree(self.tree)
+
+  def dump_words(self): 
+    #translated words
+    tw = self.tree.xpath('//div[@class="entry-content"]//td[3]//text()')
+
+    #foreign words
+    fw = self.tree.xpath('//div[@class="entry-content"]//td[2]//text()')
+
+    #pack words into dict
+    dictionary = dict(zip(tw, fw))
+
+    #store words into a json 
+    with open('scraped_words/wordz.json', 'w') as f:
+      data = json.dump(dictionary, f)
